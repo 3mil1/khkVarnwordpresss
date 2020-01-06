b1b13ef4875925663f3b0c036db8da97dd519da7
varnomasingemil <emilvarn@gmail.com>
2020-01-06 13:33:25 +0200

    modified file `config.php`

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1578914932;s:7:"authKey";s:64:"!]LY[hD|Pm5wtz)T)lhXIC!3`p>0C.-7g/#b<C|I=:VT:|g${mS9gdjr<<-V!l7z";}
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1578914932;s:7:"authKey";s:64:"!]LY[hD|Pm5wtz)T)lhXIC!3`p>0C.-7g/#b<C|I=:VT:|g${mS9gdjr<<-V!l7z";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:4000;}
